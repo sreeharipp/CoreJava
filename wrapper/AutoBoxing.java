664b1fd41e913079c576c913c754dfdd0da6ad95
sreeharipp <58019535+sreeharipp@users.noreply.github.com>
2019-12-16 18:54:27 +0530

    Wrapper class
    programs of Auto boxing and Unboxing

---
+++ wrapper/AutoBoxing.java
@@ -0,0 +1,12 @@
+package wrapper;
+
+public class AutoBoxing {
+	public static void main(String[] args)
+	{
+		int i=20;
+		Integer a=i;
+		Integer k=Integer.valueOf(i);
+		System.out.print(i+" "+a+" "+k);
+	}
+
+}
---
+++ wrapper/UnBoxing.java
@@ -0,0 +1,12 @@
+package wrapper;
+
+public class UnBoxing {
+	public static void main(String[] args)
+	{
+	Integer I=new Integer(20);
+	int k=I;
+	int a=I.intValue();
+	System.out.print(I+" "+k+" "+a);
+	}
+
+}
